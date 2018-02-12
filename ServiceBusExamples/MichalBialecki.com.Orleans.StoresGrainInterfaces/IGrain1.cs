2cb488c79af839d923636c3dd417dbd3dd62fb01
Michał Białecki <mik.bialecki@gmail.com>
2018-02-12 23:10:23 +0100

    [minor] Added project templates for Orleans

---
+++ ServiceBusExamples/MichalBialecki.com.Orleans.StoresGrainInterfaces/IGrain1.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Orleans;
+
+namespace MichalBialecki.com.Orleans.StoresGrainInterfaces
+{
+    /// <summary>
+    /// Grain interface IGrain1
+    /// </summary>
+    public interface IGrain1 : IGrainWithGuidKey
+    {
+
+    }
+}
---
+++ ServiceBusExamples/MichalBialecki.com.Orleans.StoresGrains/Grain1.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Orleans;
+
+namespace MichalBialecki.com.Orleans.StoresGrains
+{
+    /// <summary>
+    /// Grain implementation class Grain1.
+    /// </summary>
+    public class Grain1 : Grain, IGrain1
+    {
+
+    }
+}
