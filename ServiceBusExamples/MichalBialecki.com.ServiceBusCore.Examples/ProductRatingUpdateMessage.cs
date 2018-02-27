af7fe54b86080aaa18406b3e1a194b72003088d6
Michał Białecki <mik.bialecki@gmail.com>
2018-02-28 00:06:21 +0100

    [minor] Added sample project to show receiving messages

---
+++ ServiceBusExamples/MichalBialecki.com.ServiceBusCore.Examples/ProductRatingUpdateMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace MichalBialecki.com.ServiceBusCore.Examples
+{
+    public class ProductRatingUpdateMessage
+    {
+        public int ProductId { get; set; }
+
+        public int SellerId { get; set; }
+
+        public int RatingSum { get; set; }
+
+        public int RatingCount { get; set; }
+    }
+}
---
+++ ServiceBusExamples/MichalBialecki.com.ServiceBusCore.Examples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MichalBialecki.com.ServiceBusCore.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MessageReceiver().Receive();
+
+            Console.ReadKey();
+        }
+    }
+}
