946b66efee7def6c53b5b0e160f8b560159ec181
Michał Białecki <mik.bialecki@gmail.com>
2018-03-14 07:36:57 +0100

    [minor] Added singleton thread-safe class to write to azure table storage

--- ServiceBusExamples/MichalBialecki.com.ServiceBus.Examples/AccountTransferExample.cs
+++ ServiceBusExamples/MichalBialecki.com.ServiceBus.Examples/AccountTransferExample.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.Azure.CosmosDB.Table;
-using Microsoft.Azure.Storage;
-using Microsoft.ServiceBus.Messaging;
+﻿using Microsoft.ServiceBus.Messaging;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ServiceBusExamples
@@ -13,18 +9,8 @@ namespace ServiceBusExamples
     public class AccountTransferExample
     {
         private const string ServiceBusConnectionString = "Endpoint=sb://bialecki.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=39cH/mE4siF49REMd9xtjVlUwoc0yPJNz9J8isRc9vY=";
-        private const string CosmosBDConnectionString = "DefaultEndpointsProtocol=https;AccountName=bialecki-t;AccountKey=lDRHuoOFmq3g91orbG1L68YOu7M5DXRgXG8DoHU771IVvNPaijdBEyBuuNh5YY9YkJQXhTU8AJIbEEseC8ZTHg==;TableEndpoint=https://bialecki-t.table.cosmosdb.azure.com:443/;";
-        //private const string CosmosDBPrimaryKey = "6hP3HMQeuH2n1oN0cby3W7rt9uZexEGgj7TNHwLnFgQh7qKqTvmEUQPsgahpBHgXPS9W0jTLRjS4G3l2HEPsPQ==";
-        private const string CosmosDbEndpointUri = "https://bialecki-t.documents.azure.com:443/";
-        private const string TableName = "Accounts";
-        private const string PartitionKey = "Partition1";
-        private const string AccountName = "bialecki-t";
-        private const string AccountKey = "lDRHuoOFmq3g91orbG1L68YOu7M5DXRgXG8DoHU771IVvNPaijdBEyBuuNh5YY9YkJQXhTU8AJIbEEseC8ZTHg==";
-        //DefaultEndpointsProtocol=https;AccountName=bialecki-t;AccountKey=lDRHuoOFmq3g91orbG1L68YOu7M5DXRgXG8DoHU771IVvNPaijdBEyBuuNh5YY9YkJQXhTU8AJIbEEseC8ZTHg==;TableEndpoint=https://bialecki-t.table.cosmosdb.azure.com:443/;
 
         private TopicClient accountBalanceClient;
-        private CloudTable accountsTable;
-        private IEnumerable<CloudTable> tables;
 
         public void Process()
         {
@@ -36,15 +22,10 @@ namespace ServiceBusExamples
 
             accountBalanceClient = TopicClient.CreateFromConnectionString(
                 ServiceBusConnectionString, "balanceUpdates");
-            
-            var storageAccount = CloudStorageAccount.Parse(CosmosBDConnectionString);
-            var tableClient = storageAccount.CreateCloudTableClient();
-            tables = tableClient.ListTables().ToList();
-            accountsTable = tableClient.GetTableReference("accounts");
-            
+
             accountTransferClient.OnMessageAsync(
                 OnMessage,
-                new OnMessageOptions { AutoComplete = false, MaxConcurrentCalls = 1 });
+                new OnMessageOptions { AutoComplete = false, MaxConcurrentCalls = 5 });
         }
 
         private async Task OnMessage(BrokeredMessage message)
@@ -59,8 +40,8 @@ namespace ServiceBusExamples
                 var transferMessage = JsonConvert.DeserializeObject<AccountTransferMessage>(messageBody);
 
                 // get and update
-                var fromBalance = await UpdateAccount(transferMessage.From, -(double)transferMessage.Amount);
-                var toBalance = await UpdateAccount(transferMessage.To, (double)transferMessage.Amount);
+                var fromBalance = TableStorageService.Instance.UpdateAccount(transferMessage.From, -(double)transferMessage.Amount);
+                var toBalance = TableStorageService.Instance.UpdateAccount(transferMessage.To, (double)transferMessage.Amount);
 
                 // send to topic
                 var fromUpdateMessage = new BrokeredMessage(
@@ -77,33 +58,6 @@ namespace ServiceBusExamples
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-            }
-        }
-
-        private async Task<double> UpdateAccount(int accountNumber, double amount)
-        {
-            var getOperation = TableOperation.Retrieve<AccountEntity>(PartitionKey, accountNumber.ToString());
-            var result = await accountsTable.ExecuteAsync(getOperation);
-            if (result.Result != null)
-            {
-                var account = result.Result as AccountEntity;
-                account.Balance += amount;
-                var replaceOperation = TableOperation.Replace(account);
-                await accountsTable.ExecuteAsync(replaceOperation);
-
-                return account.Balance;
-            }
-            else
-            {
-                var account = new AccountEntity
-                {
-                    PartitionKey = PartitionKey,
-                    RowKey = accountNumber.ToString(),
-                    Balance = amount
-                };
-                await accountsTable.ExecuteAsync(TableOperation.Insert(account));
-
-                return amount;
             }
         }
     }
---
+++ ServiceBusExamples/MichalBialecki.com.ServiceBus.Examples/TableStorageService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.CosmosDB.Table;
+using Microsoft.Azure.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceBusExamples
+{
+    public class TableStorageService
+    {
+        private static readonly Lazy<TableStorageService> lazy =
+            new Lazy<TableStorageService>(() => new TableStorageService());
+        private static readonly object _lock = new object();
+
+        private const string CosmosBDConnectionString = "DefaultEndpointsProtocol=https;AccountName=bialecki-t;AccountKey=lDRHuoOFmq3g91orbG1L68YOu7M5DXRgXG8DoHU771IVvNPaijdBEyBuuNh5YY9YkJQXhTU8AJIbEEseC8ZTHg==;TableEndpoint=https://bialecki-t.table.cosmosdb.azure.com:443/;";
+        private const string PartitionKey = "Partition1";
+        
+        private CloudTable accountsTable;
+        
+        public static TableStorageService Instance { get { return lazy.Value; } }
+
+        public TableStorageService()
+        {
+            var storageAccount = CloudStorageAccount.Parse(CosmosBDConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            accountsTable = tableClient.GetTableReference("accounts");
+
+        }
+        
+        public double UpdateAccount(int accountNumber, double amount)
+        {
+            lock(_lock)
+            {
+                return UpdateAccountThreadSafe(accountNumber, amount);
+            }
+        }
+
+        private double UpdateAccountThreadSafe(int accountNumber, double amount)
+        {
+            var getOperation = TableOperation.Retrieve<AccountEntity>(PartitionKey, accountNumber.ToString());
+            var result = accountsTable.Execute(getOperation);
+            if (result.Result != null)
+            {
+                var account = result.Result as AccountEntity;
+                account.Balance += amount;
+                var replaceOperation = TableOperation.Replace(account);
+                accountsTable.Execute(replaceOperation);
+
+                return account.Balance;
+            }
+            else
+            {
+                var account = new AccountEntity
+                {
+                    PartitionKey = PartitionKey,
+                    RowKey = accountNumber.ToString(),
+                    Balance = amount
+                };
+                accountsTable.Execute(TableOperation.Insert(account));
+
+                return amount;
+            }
+        }
+    }
+}
